e3f58c2e6776d1dddfc1903bd60a99d5f8ec853a
Alex-Endevour <alejugger@gmail.com>
2026-03-16 22:49:42 -0600

    Add initial game logic and board setup

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 console.log("Project loaded");
+
+
+const board = {
+    cells: ["", "", "", "", "", "", "", "", ""],
+
+    reset: function () {
+        for (let index = 0; index < this.cells.length; index++) {
+            this.cells[index] = ""
+        }
+    }
+
+
+}
+
+console.log("Initial board:");
+console.log(board.cells);
+
+board.cells[0] = "X";
+board.cells[1] = "X";
+board.cells[2] = "X";
+board.cells[3] = "O";
+board.cells[4] = "X";
+board.cells[5] = "O";
+console.log(board.cells);
+board.reset();
+
+console.log("Board after reset:");
+console.log(board.cells);
